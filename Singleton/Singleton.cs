f9f7c3ecf69889fb2b66fb1364e597006441d2fb
Carlos-Henriquez <202010203@itla.edu.do>
2022-02-15 11:46:27 -0400

    Initial commit

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        public static Singleton Instancia = null;
+        public string mensaje = " ";
+
+        protected Singleton()
+        {
+            mensaje = "Vive para aprender y ciertamente aprenderas a vivir.\n \n Jhon C. Maxwell.";
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (Instancia == null)
+                    Instancia = new Singleton();
+
+                return Instancia;
+            }
+        }
+    }
+}
